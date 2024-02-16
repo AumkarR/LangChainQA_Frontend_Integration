4e81511d443627273ca03a6360b0f1fb69cab089
LiuLawrence45 <liu.lawrence45@gmail.com>
2024-02-16 13:53:06 -0800

    parallel scraping

--- backend/explore_robots.py
+++ backend/explore_robots.py
@@ -1,4 +1,4 @@
-import requests, zipfile
+import requests
 from urllib.robotparser import RobotFileParser
 import gzip
 import shutil
@@ -6,7 +6,12 @@ from io import BytesIO
 import os
 import xml.etree.ElementTree as ET
 import re
+from bs4 import BeautifulSoup
 import json
+from tqdm import tqdm
+import concurrent.futures
+from urllib.parse import urlparse #Importing urlparse to help parse url to retrieve their HTML files
+
 
 robots_url = "https://www.producthunt.com/robots.txt"
 
@@ -14,7 +19,7 @@ robots_url = "https://www.producthunt.com/robots.txt"
 robots_txt_content = requests.get(robots_url).text
 
 # make array of all urls in robots.txt  
-urls_array = [line.replace("Sitemap: ", "") for line in robots_txt_content.split('\n') if line.startswith('Sitemap')]
+overall_urls = [line.replace("Sitemap: ", "") for line in robots_txt_content.split('\n') if line.startswith('Sitemap')]
 
 
 def download_and_unzip(url):
@@ -47,24 +52,41 @@ def extract_urls_from_xml(xml_file_path, urls_array):
     
     # Array to store the URLs
     
-    # Iterate over all elements in the XML
     for elem in root.iter():
         # If the element's text contains a URL, add it to the array
         if elem.text:
             found_urls = url_pattern.findall(elem.text)
             urls_array.extend(found_urls)
-    
-    # with open('./urls.txt', 'a') as f:
-    #     f.write('\n'.join(urls))
-    # print(urls)
-    # return urls
 
-    
+def extract_name_and_description(urls_array):
+    url_data = []
 
+    def process_url(url):
+        response = requests.get(url)
+        html_content = response.text
+        soup = BeautifulSoup(html_content, "html.parser")
+        title = soup.title.string
+        description = soup.find('meta', attrs={'name': 'description'}).get('content')
+        links = [link.get('href') for link in soup.find_all('a') if link.get('href').startswith(("http://", "https://")) and not link.get('href').endswith(("producthunt", "producthunt.com", "ProductHunt")) and not link.get('href') == "https://www.youtube.com/channel/UCOtU18DT8csQVqHPT1wtYzw" and not link.get('href') == "https://product-hunt.breezy.hr/"]
+        url_data.append({
+            "url": url,
+            "title": title,
+            "description": description,
+            "links": links
+        })
 
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(tqdm(executor.map(process_url, urls_array), total=len(urls_array), leave = True))  # add a progress bar in tqdm
+
+    with open('url_data.json', 'w') as file:
+        json.dump(url_data, file, indent=4)
 
 if __name__ == "__main__":
-    for url in urls_array:
+
+    # EXTRACTING URLS
+    return_array = []
+
+    for url in overall_urls:
         xml_file = download_and_unzip(url)
         print(f"Unzipped: {xml_file}")
 
@@ -74,5 +96,22 @@ if __name__ == "__main__":
             extract_urls_from_xml(xml_file, urls_array)
         
 
-        with open('./urls_updated.txt', 'a') as f:
-            f.write('\n'.join(urls_array))
+        urls_array = [url for url in urls_array if url.startswith("https://www.producthunt.com/products/") and not url.endswith(("/reviews", "/jobs" ,"/addons"))]
+
+        # export_urls_as_html(urls_array)
+        return_array += urls_array
+
+
+    extract_name_and_description(return_array[:1024])
+
+        # with open('./urls_updated.txt', 'a') as f:
+        #     f.write('\n'.join(urls_array))
+
+
+    ## PARSING URLS
+    # with open('backend/urls_updated.txt', 'r') as f:
+    #     urls_array = f.readlines()
+    # print('\n'.join(urls_array))
+
+
+        
